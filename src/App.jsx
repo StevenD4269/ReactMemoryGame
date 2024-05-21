1045e29f1162b9a18aff7222c922286425c4714d
Dan <poddocdan@gmail.com>
2024-05-21 12:30:33 -0700

    define objectives

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+// Define an array of items
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
